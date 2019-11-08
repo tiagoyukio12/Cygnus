4435393372de8d184b6a87ca0b146e64f39c297f
Tiago Y. Fujii <tiagoyukio12@gmail.com>
2019-11-08 17:21:42 -0300

    Add window designs

---
+++ Cygnus/CVoluntario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cygnus
+{
+    public class CVoluntario
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Endereco { get; set; }
+
+        public override string ToString()
+        {
+            return this.Nome + " - " + this.DataNascimento.ToString("MM/dd/yyyy") + " - " + this.Endereco;
+        }
+    }
+}
---
+++ Cygnus/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cygnus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<CVoluntario> items = new List<CVoluntario>();
+            DateTime dataNascimento = new DateTime(2000, 01, 01);
+            items.Add(new CVoluntario() { Nome = "John Doe", DataNascimento = dataNascimento, Endereco = "Av. 1" });
+            items.Add(new CVoluntario() { Nome = "Jane Doe", DataNascimento = dataNascimento, Endereco = "Av. 1" });
+            lvDataBinding.ItemsSource = items;
+        }
+    }
+}
