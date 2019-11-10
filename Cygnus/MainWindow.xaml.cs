4a0710f0c3dd063a517b03e1c861c947d2544a8e
Tiago Y. Fujii <tiagoyukio12@gmail.com>
2019-11-10 01:35:55 -0300

    Design calendar tab

--- Cygnus/MainWindow.xaml.cs
+++ Cygnus/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Cygnus
 {
@@ -23,11 +14,69 @@ namespace Cygnus
         public MainWindow()
         {
             InitializeComponent();
+
+            // Calendar tab
+            int currentYear = 2019;
+            int currentMonth = 10;
+            CreateCalendar(currentYear, currentMonth);
+
+            // Volunteer editor tab
             List<CVoluntario> items = new List<CVoluntario>();
             DateTime dataNascimento = new DateTime(2000, 01, 01);
             items.Add(new CVoluntario() { Nome = "John Doe", DataNascimento = dataNascimento, Endereco = "Av. 1" });
             items.Add(new CVoluntario() { Nome = "Jane Doe", DataNascimento = dataNascimento, Endereco = "Av. 1" });
             lvDataBinding.ItemsSource = items;
+        }
+
+        /// <summary>
+        /// Creates daily columns for monthly calendar, and populates with events for each volunteer
+        /// </summary>
+        void CreateCalendar(int currentYear, int currentMonth)
+        {
+            int numDays = DateTime.DaysInMonth(currentYear, currentMonth);
+
+            for (int i = -1; i < numDays; i++)
+            {
+                DataGridTextColumn dataGridTextColumn = new DataGridTextColumn();
+
+                if (i == -1)
+                {
+                    dataGridTextColumn.Header = "Voluntário";
+                    dataGridTextColumn.Binding = new Binding("Name");
+                }
+                else
+                {
+                    var day = new DateTime(currentYear, currentMonth, i + 1);
+
+                    var culture = new System.Globalization.CultureInfo("pt-BR");
+                    var week_day = culture.DateTimeFormat.GetDayName(day.DayOfWeek);
+
+                    dataGridTextColumn.Header = week_day + " - " + (i + 1);
+                    dataGridTextColumn.Binding = new Binding(string.Format("Days[{0}]", i));
+                    dataGridTextColumn.CanUserSort = false;
+                }
+                dataGrid.Columns.Add(dataGridTextColumn);
+            }
+
+            CalendarBinding calendarBinding = new CalendarBinding("Miguel", numDays);
+            calendarBinding.Days[2] = "Evento";
+
+            dataGrid.Items.Add(calendarBinding);
+        }
+
+        /// <summary>
+        /// Data binding class for DataGrid in calendar tab
+        /// </summary>
+        public class CalendarBinding
+        {
+            public string Name { get; set; }
+            public string[] Days { get; set; }
+
+            public CalendarBinding(string name, int numDays)
+            {
+                Name = name;
+                Days = new string[numDays];
+            }
         }
     }
 }
