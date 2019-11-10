5d484fc335d27c1f4eea20cf4ad88cba90fd5b1d
Tiago Y. Fujii <tiagoyukio12@gmail.com>
2019-11-10 01:45:43 -0300

    Add Atividade class

---
+++ Cygnus/CAtividade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Cygnus
+{
+    class CAtividade
+    {
+        public int Codigo { get; set; }
+        public string Localizacao { get; set; }
+        public DateTime DataInicio { get; set; }
+        public int Turno { get; set; }
+        public string Frequencia { get; set; }
+
+        public override string ToString()
+        {
+            return this.Codigo.ToString();
+        }
+    }
+}
